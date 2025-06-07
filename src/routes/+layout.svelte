4d0f060387aaf072364acfd421aa4f261a9f59a6
Enzo <enzonbarata@outlook.com>
2025-06-07 18:21:47 -0300

    style & refactor: adding container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,79 +1,77 @@
-<script>
-  import { preventDefault } from 'svelte/legacy';
+<script lang="ts">
+	import '../app.css';
+	import { PUBLIC_TRAKT_ID, PUBLIC_HOSTNAME } from '$env/static/public';
+	import { Button } from '$lib/elements/ui/button';
+	import * as Avatar from '$lib/elements/ui/avatar';
+	import * as Popover from '$lib/elements/ui/popover';
+	import { Input } from '$lib/elements/ui/input';
+	import Search from 'svelte-ionicons/Search.svelte';
+	import { goto } from '$app/navigation';
 
-  import "../app.css";
-  import { PUBLIC_TRAKT_ID } from "$env/static/public";
-  import { Button } from "$lib/elements/ui/button";
-  import * as Avatar from "$lib/elements/ui/avatar";
-  import * as Popover from "$lib/elements/ui/popover";
-  import { Input } from "$lib/elements/ui/input";
-  import Search from "svelte-ionicons/Search.svelte";
-  import { goto } from "$app/navigation";
+	let { data, children } = $props();
+	let { user } = data;
 
-  /** @type {{data: any, children?: import('svelte').Snippet}} */
-  let { data, children } = $props();
-  let { user } = data;
+	async function signOut() {
+		await fetch('/out/sign');
+		goto('/out');
+	}
 
-  async function signOut() {
-    await fetch("/out/sign");
-    goto("/out");
-  }
-
-  let searchQuery = $state('');
-  async function search() {
-    goto('/search?q=' + encodeURIComponent(searchQuery));
-  }
+	let searchQuery = $state('');
+	async function search(event: SubmitEvent) {
+		event.preventDefault();
+		goto('/search?q=' + encodeURIComponent(searchQuery));
+	}
 </script>
 
-<header class="p-2 grid grid-cols-3 justify-between flex-wrap w-full z-40">
-  <form class="relative flex-1 md:grow-0" onsubmit={preventDefault(search)}>
-    <Search class="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground outline-hidden" />
-    <Input
-      type="search"
-      placeholder="Search..."
-      bind:value={searchQuery}
-      class="w-full rounded-lg bg-neutral-300 dark:bg-neutral-600 pl-8 md:w-[200px] lg:w-[320px]"
-    />
-  </form>
-  <a href="/" class="mx-auto"
-    ><img src="/logo.png" class="h-14 mx-auto" alt="ReverseTV" /></a
-  >
-  {#if user}
-    <Popover.Root>
-      <Popover.Trigger class="inline-flex ml-auto h-min">
-        <div class="flex flex-row items-center gap-2">
-          <span>{user.user.username}</span>
-          <Avatar.Root>
-            <Avatar.Image
-              alt={user.user.username}
-              src={user.user.images.avatar.full}
-            />
-            <Avatar.Fallback
-              >{user.user.username.toUpperCase()[0]}</Avatar.Fallback
-            >
-          </Avatar.Root>
-        </div>
-      </Popover.Trigger>
-      <Popover.Content class="mt-1"
-        ><Button variant="destructive" class="w-full" on:click={signOut}
-          >Sign out</Button
-        ></Popover.Content
-      >
-    </Popover.Root>
-  {:else}
-    <Button
-      href="https://trakt.tv/oauth/authorize?client_id={PUBLIC_TRAKT_ID}&redirect_uri=https://reversetv.enzon19.com/authorized&response_type=code"
-      class="ml-auto">Continue with Trakt</Button
-    >
-  {/if}
+<header class="z-40 grid w-full grid-cols-[1fr_auto_1fr] gap-2 p-2">
+	<form class="relative flex justify-start md:grow-0" onsubmit={search}>
+		<Search class="text-muted-foreground absolute top-2.5 left-2.5 h-4 w-4" />
+		<Input
+			type="search"
+			placeholder="Search..."
+			bind:value={searchQuery}
+			class="w-full max-w-48 rounded-lg bg-neutral-300 pl-8 sm:max-w-64 lg:max-w-80 dark:bg-neutral-600"
+		/>
+	</form>
+
+	<a href="/" class="justify-self-center">
+		<img src="/logo.png" class="h-14" alt="ReverseTV" />
+	</a>
+
+	<div class="justify-self-end">
+		{#if user}
+			<Popover.Root>
+				<Popover.Trigger class="inline-flex items-center gap-2">
+					<span class="max-w-[100px] truncate">{user.user.username}</span>
+					<Avatar.Root>
+						<Avatar.Image alt={user.user.username} src={user.user.images.avatar.full} />
+						<Avatar.Fallback>{user.user.username.toUpperCase()[0]}</Avatar.Fallback>
+					</Avatar.Root>
+				</Popover.Trigger>
+				<Popover.Content class="mt-1">
+					<Button variant="destructive" class="w-full" onclick={signOut}>Sign out</Button>
+				</Popover.Content>
+			</Popover.Root>
+		{:else}
+			<a
+				href="https://trakt.tv/oauth/authorize?client_id={PUBLIC_TRAKT_ID}&redirect_uri={PUBLIC_HOSTNAME}/authorized&response_type=code"
+				class="inline-block"
+			>
+				<Button class="whitespace-nowrap">Continue with Trakt</Button>
+			</a>
+		{/if}
+	</div>
 </header>
 
-<div class="grow p-2">{@render children?.()}</div>
+<div class="container mx-auto flex grow flex-col gap-6 p-2">{@render children?.()}</div>
 
-<footer class="bg-neutral-400 dark:bg-neutral-700 text-center text-sm p-2 z-30 mt-2">
-  <span>Made by <a href="https://enzon19.com" class="underline">enzon19</a>. Donate at <a class="underline" href="https://ko-fi.com/enzon19">Ko-fi</a>.</span>
+<footer class="z-30 mt-2 bg-neutral-400 p-2 text-center text-sm dark:bg-neutral-700">
+	<span
+		>Made by <a href="https://enzon19.com" class="underline">enzon19</a>. Make a donation
+		<a class="underline" href="https://enzon19.com/donate">here</a>.</span
+	>
 </footer>
 
 <style lang="postcss">
-  @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap");
+	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 </style>
