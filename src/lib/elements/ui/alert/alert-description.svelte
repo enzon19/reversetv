ab9fb26bfbe55570c50387d5d122ec2c068499ae
Enzo <enzonbarata@outlook.com>
2025-06-07 18:21:05 -0300

    refactor: using span in alert

--- src/lib/elements/ui/alert/alert-description.svelte
+++ src/lib/elements/ui/alert/alert-description.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn, type WithElementRef } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn, type WithElementRef } from '$lib/utils.js';
 
 	let {
 		ref = $bindable(null),
 		class: className,
 		children,
 		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+	}: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
 </script>
 
-<div
+<span
 	bind:this={ref}
 	data-slot="alert-description"
 	class={cn(
-		"text-muted-foreground col-start-2 grid justify-items-start gap-1 text-sm [&_p]:leading-relaxed",
+		'text-muted-foreground col-start-2 text-sm [&_p]:leading-relaxed',
 		className
 	)}
 	{...restProps}
 >
 	{@render children?.()}
-</div>
+</span>
