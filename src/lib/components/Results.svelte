919a58fed524ee06fc8ab6a91bd80743218fd8fe
Enzo <enzonbarata@outlook.com>
2025-07-06 17:02:53 -0300

    refactor: ondestroy to abort requests

--- src/lib/components/Results.svelte
+++ src/lib/components/Results.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import { getUserFullHistoryIDs } from '$lib/utils/getUserHistory';
 	import {
 		getPersonCredits,
 		getMovieCredits,
 		getShowCredits,
-		getSeasonCredits
+		getSeasonCredits,
+		getEpisodeCredits
 	} from '$lib/utils/getTMDBData';
 	import { Progress } from '$lib/elements/ui/progress/index';
 	import PeopleGrid from './PeopleGrid.svelte';
@@ -15,52 +17,67 @@
 		type,
 		itemNumber,
 		acc
-	}: { user?: TraktUser; item: any; type: Items; itemNumber?: number; acc?: string } = $props();
+	}: { user?: TraktUser; item: any; type: Items; itemNumber?: number | number[]; acc?: string } =
+		$props();
 
 	let progress: number = $state(0);
 	let progressMax: number = $state(1);
+	let data = $state();
+	let cancelled = false;
+
+	const creditsFunctions = {
+		movies: getMovieCredits,
+		shows: getShowCredits,
+		seasons: getSeasonCredits,
+		episodes: getEpisodeCredits
+	};
 
 	async function reverseTVMain() {
 		const id = item.ids.tmdb;
 		let result = [];
 
-		const creditsFunctions = {
-			movies: getMovieCredits,
-			shows: getShowCredits,
-			seasons: getSeasonCredits
-		};
 		const credits = await creditsFunctions[type](id, itemNumber);
-
+		if (cancelled) return;
 		progressMax = credits.length + 2;
 		progress += 1;
 
 		const history = await getUserFullHistoryIDs(user.username, acc);
+		if (cancelled) return;
 		progress += 1;
 
 		for (const person of credits) {
+			if (cancelled) return;
+
 			try {
 				const personCredits = await getPersonCredits(person.id);
+				if (cancelled) return;
 
 				const crossovers = personCredits.filter(
 					({ id: itemID }) =>
 						itemID !== (type === 'movies' ? 'm_' + id : 't_' + id) && history.includes(itemID)
 				);
-				if (crossovers.length > 0) result.push({ person, items: crossovers });
 
+				if (crossovers.length > 0) result.push({ person, items: crossovers });
 				progress += 1;
 			} catch (e) {
 				console.error(e);
-				throw e;
+				if (!cancelled) throw e;
 			}
 		}
 
-		// console.log(result)
-		return result;
-		// (await getPersonCredits(credits[0].id)).filter(({id}) => history.includes(id))
+		data = result;
 	}
+
+	onMount(() => {
+		reverseTVMain();
+	});
+
+	onDestroy(() => {
+		cancelled = true;
+	});
 </script>
 
-{#await reverseTVMain()}
+{#if !data}
 	<div class="mx-auto w-[50%]">
 		<span>{Math.ceil((progress / progressMax) * 100)}%</span>
 		{#if progressMax - 2 > 45}
@@ -68,6 +85,6 @@
 		{/if}
 		<Progress value={progress} max={progressMax} class="w-full" />
 	</div>
-{:then data}
+{:else}
 	<PeopleGrid {data} />
-{/await}
+{/if}
