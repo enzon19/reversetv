862887fad59caccca7207dc603cb18ea263b76d8
Enzo <enzonbarata@outlook.com>
2025-07-06 17:02:33 -0300

    fix: useless imports

--- src/routes/(items)/movies/[slug]/+page.server.ts
+++ src/routes/(items)/movies/[slug]/+page.server.ts
@@ -1,6 +1,4 @@
 import { getMovieData } from '$lib/utils/getTraktData';
-import { getShowHistory } from '$lib/utils/getUserHistory.js';
-import { redirect } from '@sveltejs/kit';
 
 export async function load({ params }) {
 	const item = await getMovieData(params.slug);
