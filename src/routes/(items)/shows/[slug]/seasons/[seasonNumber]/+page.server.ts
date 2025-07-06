e04ad9d1172db67431267be4c356c51314b36bcc
Enzo <enzonbarata@outlook.com>
2025-07-06 16:20:07 -0300

    refactor: rename param `number` to `seasonNumber`

--- src/routes/(items)/shows/[slug]/seasons/[seasonNumber]/+page.server.ts
+++ src/routes/(items)/shows/[slug]/seasons/[seasonNumber]/+page.server.ts
@@ -4,11 +4,12 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({ params, cookies }) {
 	const slug = params.slug;
-	let seasonNumber = parseInt(params.number || '1');
+	let seasonNumber = parseInt(params.seasonNumber || '1');
 
 	if (isNaN(seasonNumber)) redirect(303, '/shows/' + params.slug);
 	const showHistoryForUser = await getShowHistory(slug, cookies.get('access_token')); // for seasons, episodes + progress
-	if (seasonNumber > showHistoryForUser.seasons.length - 1 || seasonNumber < 0) redirect(303, '/shows/' + params.slug);
+	if (seasonNumber > showHistoryForUser.seasons.length - 1 || seasonNumber < 0)
+		redirect(303, '/shows/' + params.slug);
 
 	const showData = await getShowData(slug); // basic info
 
